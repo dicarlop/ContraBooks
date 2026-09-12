2a01b90d83e34ae50c098ff8dcc196ba0e88645f
dicarlop <dicarlop@protonmail.com>
2026-09-12 19:24:23 +0000

    fix: use typed Vue devtools export

--- main/registerAppLifecycleListeners.ts
+++ main/registerAppLifecycleListeners.ts
@@ -1,5 +1,5 @@
 import { app } from 'electron';
-import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer';
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
 import { Main } from '../main';
 import { rendererLog } from './helpers';
 import { emitMainProcessError } from 'backend/helpers';
@@ -28,9 +28,7 @@ export default function registerAppLifecycleListeners(main: Main) {
 
 async function installDevTools(main: Main) {
   try {
-    await installExtension(
-      VUEJS3_DEVTOOLS as Parameters<typeof installExtension>[0]
-    );
+    await installExtension(VUEJS_DEVTOOLS);
   } catch (e) {
     rendererLog(main, 'Vue Devtools failed to install', e);
   }
