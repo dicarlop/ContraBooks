5bb321bae8282235e5f2e7370abd30715cdfd80e
dicarlop <dicarlop@protonmail.com>
2026-09-12 20:08:51 +0000

    fix: type dropdown action arguments

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,6 +4,7 @@ import type { ModelNameEnum } from 'models/types';
 import type { Field, FieldType } from 'schemas/types';
 import type { QueryFilter } from 'utils/db/types';
 import type { Ref } from 'vue';
+import type { Router } from 'vue-router';
 import type { toastDurationMap } from './ui';
 
 export type DocRef<D extends Doc = Doc> = Ref<D | null>;
@@ -21,6 +22,11 @@ export interface MessageDialogOptions {
   detail?: string;
   buttons?: MessageDialogButton[];
 }
+
+export type DropdownAction = (
+  doc?: Doc,
+  router?: Router
+) => Promise<unknown> | unknown;
 
 export interface ToastOptions {
   message: string;
@@ -90,7 +96,7 @@ export type ActionGroup = {
 export type DropdownItem = {
   label: string;
   value?: string;
-  action?: () => unknown;
+  action?: DropdownAction;
   group?: string;
   component?: { template: string };
   isGroup?: boolean;
@@ -98,36 +104,3 @@ export type DropdownItem = {
 
 export type UIGroupedFields = Map<string, Map<string, Field[]>>;
 export type ExportFormat = 'csv' | 'json';
-export type PeriodKey = 'This Year' | 'This Quarter' | 'This Month' | 'YTD';
-
-export type PrintValues = {
-  print: Record<string, unknown>;
-  doc: Record<string, unknown>;
-};
-
-export interface DialogOptions {
-  title: string;
-  type?: ToastType;
-  detail?: string | string[];
-  buttons?: DialogButton[];
-}
-
-export type DialogButton = {
-  label: string;
-  action: () => unknown;
-  isPrimary?: boolean;
-  isEscape?: boolean;
-};
-
-export type GetStartedConfigItem = {
-  label: string;
-  items: {
-    key: string;
-    label: string;
-    icon: string;
-    description: string;
-    fieldname: string;
-    documentation?: string;
-    action?: () => void;
-  }[];
-};
