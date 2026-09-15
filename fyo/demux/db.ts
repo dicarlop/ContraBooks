4cb58e4a4580805d62a0b57319a18de6415a3394
dicarlop <dicarlop@protonmail.com>
2026-09-15 17:19:35 +0000

    feat: pass company file unlock key through demux

--- fyo/demux/db.ts
+++ fyo/demux/db.ts
@@ -12,71 +12,37 @@ export class DatabaseDemux extends DatabaseDemuxBase {
 
   async #handleDBCall(func: () => Promise<BackendResponse>): Promise<unknown> {
     const response = await func();
-
     if (response.error?.name) {
       const { name, message, stack } = response.error;
       const dberror = new DatabaseError(`${name}\n${message}`);
       dberror.stack = stack;
-
       throw dberror;
     }
-
     return response.data;
   }
 
   async getSchemaMap(): Promise<SchemaMap> {
-    if (!this.#isElectron) {
-      throw new NotImplemented();
-    }
-
-    return (await this.#handleDBCall(async () => {
-      return await ipc.db.getSchema();
-    })) as SchemaMap;
+    if (!this.#isElectron) throw new NotImplemented();
+    return (await this.#handleDBCall(async () => ipc.db.getSchema())) as SchemaMap;
   }
 
-  async createNewDatabase(
-    dbPath: string,
-    countryCode?: string
-  ): Promise<string> {
-    if (!this.#isElectron) {
-      throw new NotImplemented();
-    }
-
-    return (await this.#handleDBCall(async () => {
-      return ipc.db.create(dbPath, countryCode);
-    })) as string;
+  async createNewDatabase(dbPath: string, countryCode?: string, unlockKey?: string): Promise<string> {
+    if (!this.#isElectron) throw new NotImplemented();
+    return (await this.#handleDBCall(async () => ipc.db.create(dbPath, countryCode, unlockKey))) as string;
   }
 
-  async connectToDatabase(
-    dbPath: string,
-    countryCode?: string
-  ): Promise<string> {
-    if (!this.#isElectron) {
-      throw new NotImplemented();
-    }
-
-    return (await this.#handleDBCall(async () => {
-      return ipc.db.connect(dbPath, countryCode);
-    })) as string;
+  async connectToDatabase(dbPath: string, countryCode?: string, unlockKey?: string): Promise<string> {
+    if (!this.#isElectron) throw new NotImplemented();
+    return (await this.#handleDBCall(async () => ipc.db.connect(dbPath, countryCode, unlockKey))) as string;
   }
 
   async call(method: DatabaseMethod, ...args: unknown[]): Promise<unknown> {
-    if (!this.#isElectron) {
-      throw new NotImplemented();
-    }
-
-    return await this.#handleDBCall(async () => {
-      return await ipc.db.call(method, ...args);
-    });
+    if (!this.#isElectron) throw new NotImplemented();
+    return await this.#handleDBCall(async () => ipc.db.call(method, ...args));
   }
 
   async callBespoke(method: string, ...args: unknown[]): Promise<unknown> {
-    if (!this.#isElectron) {
-      throw new NotImplemented();
-    }
-
-    return await this.#handleDBCall(async () => {
-      return await ipc.db.bespoke(method, ...args);
-    });
+    if (!this.#isElectron) throw new NotImplemented();
+    return await this.#handleDBCall(async () => ipc.db.bespoke(method, ...args));
   }
 }
