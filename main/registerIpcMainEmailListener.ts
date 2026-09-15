be327bea73c24d2eef2db6be1d9c1962839c24e7
dicarlop <dicarlop@protonmail.com>
2026-09-15 03:51:13 +0000

    feat: expose PDF generation for document email

--- main/registerIpcMainEmailListener.ts
+++ main/registerIpcMainEmailListener.ts
@@ -2,12 +2,19 @@ import { ipcMain } from 'electron';
 import { IPC_ACTIONS } from '../utils/messages';
 import { sendSmtpMessage } from './email';
 import { getEmailSettings, getSmtpConfig, setEmailSettings } from './emailSettings';
+import { renderHtmlAsPdf } from './saveHtmlAsPdf';
 
 export default function registerIpcMainEmailListener() {
   ipcMain.handle(
     IPC_ACTIONS.SEND_DOCUMENT_EMAIL,
     async (_, message: Parameters<typeof sendSmtpMessage>[1]) =>
       sendSmtpMessage(getSmtpConfig(), message)
+  );
+
+  ipcMain.handle(
+    IPC_ACTIONS.CREATE_PDF_FROM_HTML,
+    async (_, html: string, width: number, height: number) =>
+      renderHtmlAsPdf(html, require('electron').app, width, height)
   );
 
   ipcMain.handle(IPC_ACTIONS.GET_EMAIL_SETTINGS, () => getEmailSettings());
