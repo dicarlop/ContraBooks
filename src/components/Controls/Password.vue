d14eff98cda6f987995b26e8fe7c61853a828c56
dicarlop <dicarlop@protonmail.com>
2026-09-15 03:14:20 +0000

    feat: add password control for email settings

---
+++ src/components/Controls/Password.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Base from './Base.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Password',
+  extends: Base,
+  computed: {
+    inputType() {
+      return 'password';
+    },
+  },
+});
+</script>
