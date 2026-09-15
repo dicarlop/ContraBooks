14782f96f87ca7f092441dbebb27d7374de2c217
dicarlop <dicarlop@protonmail.com>
2026-09-15 15:28:36 +0000

    feat: add email history navigation from print view

--- src/pages/PrintView/PrintView.vue
+++ src/pages/PrintView/PrintView.vue
@@ -207,6 +207,13 @@ export default defineComponent({
           },
         },
         {
+          label: this.t`Email History`,
+          group: this.t`View`,
+          action: async () => {
+            await routeTo('/list/EmailLog');
+          },
+        },
+        {
           label: this.t`New Template`,
           group: this.t`Create`,
           action: async () => {
@@ -340,7 +347,7 @@ export default defineComponent({
         getPDF: () => Promise<Uint8Array | null>;
       };
 
-      if (!printContainer?.getPDF || !this.emailContext) {
+      if (!printContainer?.getPDF) {
         return;
       }
 
@@ -353,8 +360,8 @@ export default defineComponent({
 
         await ipc.sendDocumentEmail({
           ...message,
-          documentType: this.emailContext.documentType,
-          documentNumber: this.emailContext.documentNumber,
+          documentType: this.emailContext?.documentType,
+          documentNumber: this.emailContext?.documentNumber,
           attachments: [
             {
               filename: `${this.doc?.name ?? this.name}.pdf`,
