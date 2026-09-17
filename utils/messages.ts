95f430f1d00e768135aecfab59bfb48d9571a50a
dicarlop <dicarlop@protonmail.com>
2026-09-17 18:14:32 +0000

    add template designer window IPC action

--- utils/messages.ts
+++ utils/messages.ts
@@ -45,6 +45,7 @@ export enum IPC_ACTIONS {
   DB_CALL = 'db-call',
   DB_BESPOKE = 'db-bespoke',
   DB_SCHEMA = 'db-schema',
+  OPEN_TEMPLATE_DESIGNER = 'open-template-designer',
 }
 
 // ipcMain.send(...)
