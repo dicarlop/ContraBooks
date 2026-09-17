ee070b69fc18a434c6155619b2d82ee3969ff231
dicarlop <dicarlop@protonmail.com>
2026-09-17 18:03:25 +0000

    restore POS secondary quick edit route

--- src/router.ts
+++ src/router.ts
@@ -77,7 +77,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/pos',
     name: 'Point of Sale',
-    component: POS,
+    components: { default: POS, edit: QuickEditForm },
     props: { default: true, edit: (route) => route.query },
   },
 ];
